9ad01e5ff41604a7b6a93aeeca8ba818dec920dd
ReddyTintaya <readyold@icloud.com>
2019-12-27 14:15:53 -0400

    bootstrap added

--- request-response/main.py
+++ request-response/main.py
@@ -1,7 +1,16 @@
 from flask import Flask, request, make_response, redirect, render_template
-
+from flask_bootstrap import Bootstrap
 todos = ['comprar cafe', 'enviar solicitud de compra', 'TODO 3', 'TODO 4', 'TODO 5']
 app = Flask(__name__)
+bootstrap = Bootstrap(app) 
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def not_found500(error):
+    return render_template('500.html', error=error)
 
 @app.route('/')
 def index():

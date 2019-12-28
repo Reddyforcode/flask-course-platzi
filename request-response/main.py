c1258be4c3d915f02bb53f3ad6accddd73dfd24a
ReddyTintaya <readyold@icloud.com>
2019-12-28 00:03:03 -0400

    clase 17 WTF and Bootstrap

--- request-response/main.py
+++ request-response/main.py
@@ -1,10 +1,20 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 todos = ['comprar cafe', 'enviar solicitud de compra', 'TODO 3', 'TODO 4', 'TODO 5']
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 bootstrap = Bootstrap(app) 
 
+class Loginform(FlaskForm):
+    # campo ara username and pass
+    username = StringField('Nombre de Usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField("Enviar")
+    
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
@@ -25,12 +35,11 @@ def index():
 def hello():
     #user_ip = request.cookies.get('user_ip')
     user_ip = session.get('user_ip')
+    login_form = Loginform()
     context = {
         'user_ip': user_ip,
-        'todos': todos
+        'todos': todos,
+        'login_form': login_form
     }
     #return 'Hello World from {} ip'.format(user_ip)
     return render_template( 'hello.html', **context) 
-
-
-

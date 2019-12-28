37357c9a56cefa076f4421d62c192d189d58aae4
ReddyTintaya <readyold@icloud.com>
2019-12-28 00:24:57 -0400

    class 19  flask flash added

--- request-response/main.py
+++ request-response/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
@@ -14,7 +14,7 @@ class Loginform(FlaskForm):
     username = StringField('Nombre de Usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField("Enviar")
-    
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
@@ -31,15 +31,23 @@ def index():
     session['user_ip']=user_ip
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     #user_ip = request.cookies.get('user_ip')
     user_ip = session.get('user_ip')
     login_form = Loginform()
+    username = session.get('username')
     context = {
         'user_ip': user_ip,
         'todos': todos,
-        'login_form': login_form
+        'login_form': login_form,
+        'username': username
     }
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+        flash('Nombre de usuario registrado con exito')
+        return redirect(url_for('index'))
+        # dete ta cuando se manda un post y valida una forma
     #return 'Hello World from {} ip'.format(user_ip)
     return render_template( 'hello.html', **context) 
